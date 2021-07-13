d545cc40283c2d6bb1e326f277112548f9e83776
B-JayU <qudwn8712@naver.com>
2021-07-13 13:14:39 +0900

    Add files via upload

---
+++ 08. 서블릿/계산기 서블릿 구현/src/main/webapp/calc_servlet/CalcServlet.java
@@ -0,0 +1,87 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcServlet
+ */
+@WebServlet(description = "Calc1 서블릿", urlPatterns = { "/CalcServlet" })
+public class CalcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		// 변수 선언
+		int num1, num2, result;
+		String op;
+		
+		// 클라이언트 응답시 전달될 컨텐트에 대한 타입 설정과 캐릭터셋 지정
+		response.setContentType("text/html; charset=UTF-8");
+		
+		// 클라이언트 응답을 위해 출력 스트림 확보.
+		PrintWriter out = response.getWriter();
+		
+		// HTML 폼을 통해 전달된 num1, num2 파라미터 값을 변수에 할당한다.
+		// 이때 getParameter() 메서드는 문자열을 리턴하므로 숫자형 데이터의 경우 Integer.parseInt() 를 통해 형변환을 한다.
+		num1 = Integer.parseInt(request.getParameter("num1"));
+		num2 = Integer.parseInt(request.getParameter("num2"));
+		op = request.getParameter("operator");
+		
+		// calc 메서드를 호출로 결과 받음.
+		result = calc(num1, num2,op);
+		
+		// 출력 트림을 통한 화면 구성
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head><title>계산기</title></head>");
+		out.println("<body>");
+		out.println("<h2>계산결과</h2>");
+		out.println("<hr />");
+		out.println(num1 + " " + op + " " + " = " + result);
+	}
+	
+	public int calc(int num1, int num2, String op) {
+		int result = 0;
+		
+		if (op.equals("+"))
+			result = num1 + num2;
+		else if(op.equals("-"))
+			result = num1 - num2;
+		else if(op.equals("*"))
+			result = num1 * num2;
+		else if (op.equals("/"))
+			result = num1 / num2;
+		
+		return result;
+	}
+
+}
